5a82d583f48e505ea7e585b52d4793e34cb82a7b
mahsa.malmir769 <mahsa.malmir769@gmail.com>
2022-09-10 10:11:52 +0430

    use boolean and strng types

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,15 +1,34 @@
-import React, { useState } from "react";
-
+import { useState } from "react";
 const Test = () => {
-  const [data, setData] = useState<number>();
-  function add(num1: number, num2: number) {
+  const [data, setData] = useState<string>();
+  function calender(
+    num1: number,
+    num2: number,
+    showResults: boolean,
+    phrase: string
+  ) {
     //specify type
-    return setData(num1 + num2);
+    if (typeof num1 !== "number" && typeof num2 !== "number") {
+      throw new Error(" wrong type babe !!");
+    }
+    const m = num1 + num2;
+    const n = num1 - num2;
+    if (showResults) {
+      return setData(phrase + m);
+    } else {
+      return setData(phrase + n);
+    }
   }
   return (
     <div>
-      <button onClick={() => add(1, 2)} className="btn">
-        ADD
+      <button onClick={() => calender(6, 4, true, "sum is : ")} className="btn">
+        SUM
+      </button>
+      <button
+        onClick={() => calender(6, 4, false, "sub is : ")}
+        className="btn"
+      >
+        SUB
       </button>
       {/*without this in onClick ()=> we have error*/}
       <div className="data">{data}</div>
