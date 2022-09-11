73f9d65ad1f56f08713a6cd9c0e5406faccd1286
mahsa.malmir769 <mahsa.malmir769@gmail.com>
2022-09-11 15:58:20 +0430

    write literal with union & enum

--- src/pages/Union.tsx
+++ src/pages/Union.tsx
@@ -2,10 +2,26 @@ import { useState } from "react";
 
 const Union = () => {
   const [data, setData] = useState<string | number>();
-  function calender(num1: number | string, num2: number | string) {
+
+  enum Types {
+    IS_NUMBER = "is_num",
+    IS_STRING = "is_str",
+  }
+  //third param is literal and change if whit this
+  //i write this with literal && union && enum
+  function calender(
+    num1: number | string,
+    num2: number | string,
+    type: Types.IS_NUMBER | Types.IS_STRING
+  ) {
     let res;
-    if (typeof num1 === "number" && typeof num2 === "number") {
-      res = num1 + num2;
+    // if (typeof num1 === "number" && typeof num2 === "number") {
+    //   res = num1 + num2;
+    // } else {
+    //   res = num1.toString() + num2.toString();
+    // }
+    if (type === Types.IS_NUMBER) {
+      res = +num1 + +num2;
     } else {
       res = num1.toString() + num2.toString();
     }
@@ -14,10 +30,19 @@ const Union = () => {
 
   return (
     <div>
-      <button onClick={() => calender(12, 4)} className="btn">
+      <button onClick={() => calender(12, 4, Types.IS_NUMBER)} className="btn">
         number
       </button>
-      <button onClick={() => calender("12", "4")} className="btn">
+      <button
+        onClick={() => calender("12", "4", Types.IS_NUMBER)}
+        className="btn"
+      >
+        num to str
+      </button>
+      <button
+        onClick={() => calender("mahsa", "M", Types.IS_STRING)}
+        className="btn"
+      >
         string
       </button>
       <div className="data">{data}</div>
