cb840361735227b8727cad7772634e9b6b37856d
mahsa.malmir769 <mahsa.malmir769@gmail.com>
2022-09-11 17:26:54 +0430

    callback function

--- src/pages/FunctionType.tsx
+++ src/pages/FunctionType.tsx
@@ -1,10 +1,11 @@
 const FunctionType = () => {
-  function Calender(a: number, b: number) {
-    return a + b;
+  function Calender(a: number, b: number, cb: (num: number) => any) {
+    const res = a + b;
+    return cb(res);
   }
-  let sum: (a: number, b: number) => number; //function types
+  let sum: (a: number, b: number, cb: (num: number) => void) => number;
   sum = Calender;
-  return <div className="data">{sum(2, 4)}</div>;
+  return <div className="data">{sum(2, 4, () => {})}</div>;
 };
 
 export default FunctionType;
